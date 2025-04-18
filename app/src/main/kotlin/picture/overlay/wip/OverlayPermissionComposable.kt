dc8e57ce47a06b8c9293123deaafdae1ba70a664
James HN Sears <james.hn.sears@gmail.com>
2025-04-18 10:21:38 +0300

    @Preview

--- app/src/main/kotlin/picture/overlay/wip/OverlayPermissionComposable.kt
+++ app/src/main/kotlin/picture/overlay/wip/OverlayPermissionComposable.kt
@@ -1,6 +1,5 @@
 package picture.overlay.wip
 
-import android.app.Activity
 import android.content.Intent
 import android.provider.Settings
 import androidx.activity.compose.rememberLauncherForActivityResult
@@ -18,11 +17,12 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.core.net.toUri
 
-
+@Preview
 @Composable
 fun OverlayPermissionScreen(
     viewModel: OverlayPermissionViewModel = viewModel()
